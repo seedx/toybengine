5a826d0ec560cb1137dfd5e3174774c04dbe96cc
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2015-08-18 00:05:57 +0300

    add css_parser stub; update cmake file

---
+++ css_parser.h
@@ -0,0 +1,49 @@
+#ifndef TOYBENGINE_PARSER_H
+#define TOYBENGINE_PARSER_H
+
+class CSSRule {
+public:
+    std::string Selector;
+    std::vector<CSSProperty> Properties;
+    CSSRule() {
+        Properties = new std::vector<CSSProperty>();
+    }
+};
+
+class CSSProperty {
+public:
+    std::string name;
+    std::string value;
+};
+
+class Parser {
+    public:
+    static std::vector<CSSRule>* Parse(std::string* source) {
+        auto rules = new std::vector<CSSRule>();
+        std::size_t len = source->length(); 
+        std::size_t i = 0;
+        std::size_t opn_br_pos = -1;
+        opn_br_pos = source.find("{", i);
+
+        if (opn_br_pos == str::npos) {
+            // TODO: implement log
+            log_warning("invalid css after %d", i);
+        }
+
+        std::string selector = source.substr(i, opn_br_pos);
+        CSSRule rule = new CSSRule();
+        rule->Selector = selector;
+        std::size_t cls_br_pos = source.find("}", curr);
+
+        if (cls_br_pos == str::npos) {
+            log_warning("invalid css after %d", curr);
+        }
+
+        rule->properties;
+        
+        return rules;
+    } 
+};
+
+
+#endif //TOYBENGINE_PARSER_H
